3015424b64a4797505ac237f59307173a4adb3cc
mazurenka <an.drej@mail.ru>
2022-03-16 14:36:46 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,22 @@
+let http = require('http');
+
+
+let server = http.createServer( (req, res) => {
+    console.log('some request')
+} );
+
+server.listen(1234)
+
+
+
+
+
+
+
+
+console.log(http);
+
+
+
+
+
