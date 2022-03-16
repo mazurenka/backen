c5f90e5be6ea16c1b1f89e1fe1983b97a18465c2
mazurenka <an.drej@mail.ru>
2022-03-16 15:13:31 +0100

    1

--- index.js
+++ index.js
@@ -1,17 +1,12 @@
 let http = require('http');
 
 
-let server = http.createServer( (req, res) => {
+let server = http.createServer((req, res) => {
     console.log('some request')
-} );
+    res.end();
+});
 
 server.listen(1234)
-
-
-
-
-
-
 
 
 console.log(http);
